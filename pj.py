d920c0fd03b04d73f05db5183e693a0ca1788767
Shinolr <wyqbeta@gmail.com>
2021-12-15 09:15:23 +0800

    update script

--- pj.py
+++ pj.py
@@ -12,36 +12,24 @@ class PrintDataAsJSONString(fb.FBCommand):
     def description(self):
         return "Print JSON representation of Swift Data"
 
-    def options(self):
-        return [
-            fb.FBCommandArgument(
-                arg="plain",
-                short="-p",
-                long="--plain",
-                boolean=True,
-                default=False,
-                help="Plain JSON",
-            )
-        ]
-
     def args(self):
         return [
             fb.FBCommandArgument(
-                arg="object",
-                type="NSObject *",
-                help="The Swift Dictionary or Swift Array to print",
+                arg="data",
+                type="Data",
+                help="Data that will be evaluated when deserializing.",
             )
         ]
 
     def run(self, arguments, options):
         # Convert to NSObject first to allow for objc runtime to process it
-        nsData = fb.evaluateInputExpression(
+        objectToPrint = fb.evaluateInputExpression(
             "{obj} as NSObject".format(obj=arguments[0])
         )
 
         jsonString = fb.evaluateExpressionValue(
-            "(id)[[NSString alloc] initWithData:(NSData*){} encoding:4]".format(
-               nsData
+            "(NSString*)[[NSString alloc] initWithData:(NSObject*){} encoding:4]".format(
+               objectToPrint
             )
         ).GetObjectDescription()
 
